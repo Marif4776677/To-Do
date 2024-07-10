5d5ce13ae67ca160582fc31338d5a6c433bba47c
Marif4776677 Ismayilov <marifismayilovv2+demo2@gmail.com>
2024-07-10 12:58:24 +0000

    fix

--- app.js
+++ app.js
@@ -3,18 +3,9 @@ $(document).ready(function () {
         e.preventDefault();
         if (!$.trim($('input').val()) == '') {
             let a = b => {
-                for (let i = 0; i < b; i++) {
-                    $('.box').append(`<h2>${b}</h2>`);
-                }
+                for (let i = 0; i < b; i++) {  $('.box').append(`<h2>${b}</h2>`); }
             };
-            let num = y => {
-                if (Number(y)) {
-                    return a(y);
-                }
-                else {
-                    $('.box').append(`<h2>${y}</h2>`);
-                }
-            };
+            let num = y => Number(y) ? a(y): $('.box').append(`<h2>${y}</h2>`);
             num($('input').val());
         }
         $('input').val('');
